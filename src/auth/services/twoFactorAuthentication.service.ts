16ad76ce2f03a4d93a20347c47f5822e8005c9be
ThaoTruong <truongdinhthao127@gmail.com>
2024-01-28 17:09:38 +0700

    2fa

--- src/auth/services/twoFactorAuthentication.service.ts
+++ src/auth/services/twoFactorAuthentication.service.ts
@@ -1,7 +1,7 @@
 import { Environments } from '@/configs';
 import { UserService } from '@/module/user/services';
 import { Injectable } from '@nestjs/common';
-import { authenticator } from 'otplib';
+import { authenticator, totp } from 'otplib';
 import { toFileStream } from 'qrcode';
 
 @Injectable()
@@ -18,10 +18,11 @@ export class TwoFactorAuthenticationService {
   }
 
   async sendOtp(secret: string) {
+    authenticator.options = { step: 30, digits: 6, window: 2 };
     return authenticator.generate(secret);
   }
 
   async isTwoFactorAuthenticationCodeValid(code: string, secret: string) {
-    return authenticator.verify({ token: code, secret: secret });
+    return authenticator.check(code, secret);
   }
 }
